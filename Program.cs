e666cf593e67ac8fb3bc969df1f76bd9fa9591a9
Sohail Mehmood <sohail.mehmood@worldline.com>
2022-09-01 16:42:33 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Odd;
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        if (args is null)
+        {
+            throw new ArgumentNullException(nameof(args));
+        }
+
+        var numbers = new List<int>()
+            { 107, 92, 3, 46, 5096, 666, 85 };
+
+        for (int i = 0; i < numbers.Count; i += 2)
+        {
+            Console.WriteLine(numbers[i]);
+        }
+    }
+}
